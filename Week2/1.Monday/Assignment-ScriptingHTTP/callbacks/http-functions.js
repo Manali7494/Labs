401a7310a75b697f2156ca3aa756cf2864973220
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-14 19:36:03 +0000

    added new empty functions and previous module for step 6

---
+++ Week2/1.Monday/Assignment-ScriptingHTTP/callbacks/http-functions.js
@@ -0,0 +1,13 @@
+var https = require('https');
+module.exports = function getHTML (options, callback){
+  https.get(options, function(response){
+    response.setEncoding('utf8');
+    var body = "";
+    response.on('data', function(data){
+      body += data;
+    });
+    response.on('end', function(){
+      callback(body);
+    });
+  });
+};
