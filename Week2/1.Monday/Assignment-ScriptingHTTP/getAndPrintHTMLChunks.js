028e564823dd970729481f8df3249f8af6866b7b
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-14 17:16:05 +0000

    step 1 - Printing in Chunks

--- Week2/1.Monday/Assignment-ScriptingHTTP/getAndPrintHTMLChunks.js
+++ Week2/1.Monday/Assignment-ScriptingHTTP/getAndPrintHTMLChunks.js
@@ -1,6 +1,17 @@
 
+var https = require('https');
+
 function getAndPrintHTMLChunks (){
+  var reqOptions = {
+    host: 'syntaris.github.io',
+    path: '/http-examples/step1.html'
+  };
 
-
-
+  https.get(reqOptions, function(response){
+    response.setEncoding('utf8');
+    response.on('data', function(data){
+      console.log(data + "\n");
+    });
+  });
 }
+getAndPrintHTMLChunks();
