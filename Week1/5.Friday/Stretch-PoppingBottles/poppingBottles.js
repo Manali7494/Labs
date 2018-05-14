27c965d30068c1e3329a555423725512ef7140d5
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-14 06:25:46 +0000

    completed task 4 to output a string with earned bottles and caps

--- Week1/5.Friday/Stretch-PoppingBottles/poppingBottles.js
+++ Week1/5.Friday/Stretch-PoppingBottles/poppingBottles.js
@@ -4,6 +4,8 @@ function popBottles(dollars){
   var emptyBottles = 0;
   var caps = 0;
   var array = [];
+  var obtainedBottles = 0;
+  var obtainedCaps = 0;
   while (fullBottles > 0){
     array.push(fullBottles);
     if (fullBottles > 0){
@@ -16,18 +18,22 @@ function popBottles(dollars){
       tempFB += Math.floor(emptyBottles / 2);
       fullBottles += tempFB;
       emptyBottles -= tempFB * 2;
+      obtainedBottles += tempFB;
     }
     if (caps / 4 > 0){
       var temp2FB = 0;
       temp2FB = Math.floor(caps / 4);
       fullBottles += temp2FB;
       caps -= (temp2FB * 4);
+      obtainedCaps += temp2FB;
     }
   }
   var sum = array.reduce(function(sum, value){
     return sum + value;
   });
-  console.log(sum);
+
+  var string = "TOTAL BOTTLES: " + sum + "\n" + "TOTAL EARNED: \n BOTTLES: " + obtainedBottles + "\n CAPS: " + obtainedCaps;
+  console.log(string);
 }
 
 var dollars = process.argv[2];
