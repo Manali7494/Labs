a3249c03fd25167576859bb8516eb96eadbe8edf
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-14 04:44:03 +0000

    completed task 1 of exercise

---
+++ Week1/5.Friday/Stretch-PoppingBottles/poppingBottles.js
@@ -0,0 +1,12 @@
+
+function popBottles(dollars){
+
+  var numBottles = Math.floor(dollars / 2);
+
+  var emptyBottles = numBottles;
+  var bottleCaps = numBottles;
+  return numBottles;
+}
+
+console.log(popBottles(20));
+
