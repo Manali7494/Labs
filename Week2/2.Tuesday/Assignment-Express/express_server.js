317813fd7ec792430a9c87fd527bfefb6cdfcbec
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-15 20:56:27 +0000

    added express assignment

---
+++ Week2/2.Tuesday/Assignment-Express/express_server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080; // default port setup
+
+var urlDataBase = {
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9s5m5xK': 'http://www.google.com'
+};
+
+
+// Identifies path and action
+app.get("/", (request, response) => {
+  response.end("Hello!");
+});
+
+app.get("/urls.json", (request, response) => {
+  response.json(urlDataBase); // .json ensures proper display on the page
+});
+
+
+app.get('/hello', (request,response) => {
+  response.end("<html><body>Hello <b>World</b></body></html>\n")
+});
+
+// Listening to port
+app.listen(PORT, () => {
+  console.log(`Example app is listening to port ${PORT}!`);
+});
+
+
