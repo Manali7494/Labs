5a38cb53d51ac422bee6c1acc6df20c9b72c446a
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-30 01:46:59 +0000

    knex initalization for migration

---
+++ Week4/2.Tuesday/Assignment-Knex/knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+const settings = require("./settings.json");
+
+module.exports = {
+
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.passsword
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
