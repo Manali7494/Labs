cf372476d4ab1777daa719c49dc943c4d94bf0de
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-05 17:01:11 -0600

    modified code to include get and set methods

--- Week5/2.Tuesday/Exercise-Pizza/pizza.js
+++ Week5/2.Tuesday/Exercise-Pizza/pizza.js
@@ -1,19 +1,38 @@
 class Pizza {
-  constructor(size, crust) {
-    this.size = size;
+  constructor(crust) {
     this.crust = crust;
     this.toppings = ["cheese"];
   }
   addTopping(topping) {
     this.toppings.push(topping);
   }
+  // Note: When setting values, the function and the propety can not be used
+  // Therefore: '_' feature was used instead
+  set size(size) {
+    if (size === "s" || size === "m" || size === "l") {
+      this._size = size;
+    }
+  }
+
+  get price() {
+    const basePrice = 10;
+    const toppingPrice = 2;
+    return basePrice + this.toppings.length * toppingPrice;
+  }
 }
 
-let pizza1 = new Pizza("large", "thin");
-let pizza2 = new Pizza("large", "thick");
-pizza1.addTopping("chicken");
-pizza1.addTopping("pineapple");
+let pizza1 = new Pizza("thin");
+let pizza2 = new Pizza("thick");
 
-// console.log(pizza1.toppings);
-console.log(pizza1);
-console.log(pizza2);
+// Setting toppings
+pizza1.addTopping("japalpeno");
+pizza1.addTopping("onions");
+
+// Getting price
+// console.log(`Price is $${pizza1.price}`);
+// console.log(`Price is $${pizza2.price}`);
+
+// Set Size
+console.log(pizza1.price);
+pizza1.size = "s";
+console.log(pizza1._size);
