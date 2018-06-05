2863ffbd0d05bba356288673c365d881b17bccd8
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-05 15:41:31 -0600

    Added exercise for class based objects

---
+++ Week5/2.Tuesday/Exercise-Classes/pizza.js
@@ -0,0 +1,20 @@
+class Pizza {
+  constructor(size, crust) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings = ["cheese"];
+  }
+  addTopping(topping) {
+    this.toppings.push(topping);
+  }
+}
+
+let pizza1 = new Pizza('large', 'thin');
+let pizza2 = new Pizza('large','thick');
+pizza1.addTopping("chicken");
+pizza1.addTopping("pineapple");
+
+
+// console.log(pizza1.toppings);
+console.log(pizza1);
+console.log(pizza2);
