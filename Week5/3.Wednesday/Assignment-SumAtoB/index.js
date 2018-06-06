88a733c4766b1b2a0f08f2cf2b8111255b9ffadb
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-06 13:12:51 -0600

    Updated function to sum numbers with recursion

--- Week5/3.Wednesday/Assignment-SumAtoB/index.js
+++ Week5/3.Wednesday/Assignment-SumAtoB/index.js
@@ -1,5 +1,14 @@
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+
+  // Base case
+  if (fromN === toN) {
+    return fromN;
+  }
+  // Recursive Case
+  if (fromN !== toN && fromN < toN) {
+    return sum(fromN + 1, toN) + fromN;
+  }
 }
 
 module.exports = sum;
