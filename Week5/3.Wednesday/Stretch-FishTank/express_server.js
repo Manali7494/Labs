e19f77d4559def320296e58f8d1eb57f30f5552b
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-06 11:33:31 -0600

    Re-initiated the files for fishtank assignment

--- Week5/3.Wednesday/Stretch-FishTank/express_server.js
+++ Week5/3.Wednesday/Stretch-FishTank/express_server.js
@@ -1,10 +1,12 @@
-var express = require("express");
+var express = require('express');
 
-var PORT = process.env.PORT || 8080;
+
+var PORT = process.env.PORT || 8080
+
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.listen(8080, () => {
   console.log(`Listening on port ${PORT}`);
