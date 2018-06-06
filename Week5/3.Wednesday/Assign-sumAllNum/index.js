30a03c3d8ef560ceca9e12c47938ee94643c0c91
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-06 14:33:20 -0600

    updated sumItems function with recursion

--- Week5/3.Wednesday/Assign-sumAllNum/index.js
+++ Week5/3.Wednesday/Assign-sumAllNum/index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  return array.reduce(function fn(sum, item) {
+    if (Array.isArray(item)) {
+      return item.reduce(fn, sum);
+    } else if (item === Math.round(item)) {
+      return sum + item;
+    }
+    return item;
+  }, 0);
 }
 
 module.exports = sumItems;
