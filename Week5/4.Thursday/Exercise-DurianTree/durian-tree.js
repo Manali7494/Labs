3ba67747d7b17a720479181f4baab7697ba7559e
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-07 13:41:05 -0600

    Added tree traversal (Depth first traversal)

--- Week5/4.Thursday/Exercise-DurianTree/durian-tree.js
+++ Week5/4.Thursday/Exercise-DurianTree/durian-tree.js
@@ -28,6 +28,33 @@ class Employee {
   hasSameBoss(employee) {
     return this.boss === employee.boss;
   }
+
+  employeesThatMakeOver(amount) {
+    let employees = [];
+
+    if (this.salary > amount) {
+      employees.push(this);
+    }
+
+    for (const subordinate of this.subordinates) {
+      const subordinatesThatMakeOver = subordinate.employeesThatMakeOver(
+        amount
+      );
+      employees = employees.concat(subordinatesThatMakeOver);
+    }
+    return employees;
+  }
+
+  get totalEmployees() {
+    let total = 0;
+    if (this.name !== null) {
+      total++;
+    }
+    for (const indvEmployee of this.subordinates) {
+      total = total + indvEmployee.totalEmployees;
+    }
+    return total;
+  }
 }
 
 // Completing Tree Data Structure
@@ -44,22 +71,40 @@ ada.addSubordinate(angela);
 ada.addSubordinate(phil);
 
 // Craig's subordinates
-const simon = new Employee("Simone", "worker", 50000);
-const ali = new Employee("Ali", "worker", 50000);
+const simon = new Employee("Simone", "worker", 200000);
+const ali = new Employee("Ali", "worker", 100000);
 craig.addSubordinate(simon);
 craig.addSubordinate(ali);
 
+//Add Ali's subordinates
+const sarah = new Employee("Sarah", "worker", 95000);
+const andrew = new Employee("Andrew", "worker", 90000);
+
+ali.addSubordinate(sarah);
+ali.addSubordinate(andrew);
+
 // Phil's subordinates
-const florida = new Employee("Florida", "worker", 50000);
-const david = new Employee("david", "worker", 50000);
-const brian = new Employee("brian", "worker", 50000);
+const florida = new Employee("Florida", "worker", 150000);
+const david = new Employee("david", "worker", 200000);
+const brian = new Employee("brian", "worker", 100000);
 phil.addSubordinate(florida);
 phil.addSubordinate(david);
 phil.addSubordinate(brian);
 
+// add David's subordinates
+const emma = new Employee("Emma", "worker", 60000);
+const jeremy = new Employee("Jeremy", "worker", 60000);
+
+david.addSubordinate(emma);
+david.addSubordinate(jeremy);
+
 // Angela's subordinates
-const karla = new Employee("karla", "worker", 50000);
+const karla = new Employee("karla", "worker", 120000);
 angela.addSubordinate(karla);
+
+// Add Karla's subordinate
+const chandler = new Employee("Chandler", "worker", 100000);
+karla.addSubordinate(chandler);
 
 // Feature 1 - Employee's Boss
 console.log(karla.boss);
@@ -72,3 +117,12 @@ console.log(david.numberOfPeopleTOCEO);
 
 // Feature 4 - Has same boss
 console.log(david.hasSameBoss(brian));
+
+// Feature6 - Find employees that make over a certain amount
+let wealthyEmployees = ada.employeesThatMakeOver(418401);
+console.log(wealthyEmployees);
+
+// Feature 7 - Total employees under certain employee
+console.log(craig.totalEmployees);
+console.log(ada.totalEmployees);
+
