18378988a4e99dd0ff4d4b801993c1f42611bdcd
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-08 14:45:48 -0600

    Linear time with sum function

--- Week5/5.Friday/Assignment-Quadratic/sum.js
+++ Week5/5.Friday/Assignment-Quadratic/sum.js
@@ -1,21 +1,42 @@
-// Quadratic Time function for sum
+// // Quadratic Time function for sum
+// function arrayContainsSum(array, sum) {
+//   for (let i = 0; // 1
+//      i < array.length; // n + 1
+//       i++) { // n
+//     const element1 = array[i]; // n
+
+//     for (let ii = 0; // 1
+//          ii < array.length; // n + n^2
+//           i++) { // n^2
+//       const element2 = array[ii]; // n^2
+
+//       if (element1 + element2 === sum)  // n^2
+//       {
+//         return true;
+//       }
+//     }
+//   }
+//   return false; // 2
+// }
+// // Running Time: 4n^2 + 5n + 4
+
+// Requirement: expects array to be sorted
 function arrayContainsSum(array, sum) {
-  for (let i = 0; // 1
-     i < array.length; // n + 1
-      i++) { // n
-    const element1 = array[i]; // n
+  let i = 0;
+  let ii = array.length - 1;
 
-    for (let ii = 0; // 1
-         ii < array.length; // n + n^2 
-          i++) { // n^2
-      const element2 = array[ii]; // n^2
+  while (i <= ii) {
+    const element1 = array[i];
+    const element2 = array[ii];
+    const currentSum = element1 + element2;
 
-      if (element1 + element2 === sum)  // n^2
-      {
-        return true;
-      }
+    if (currentSum === sum) {
+      return true;
+    } else if (currentSum > sum) {
+      ii--;
+    } else {
+      i++;
     }
   }
-  return false; // 2
+  return false;
 }
-// Running Time: 4n^2 + 5n + 4
