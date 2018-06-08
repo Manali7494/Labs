4f468218660d43bf09884324c258b37fdf07def4
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-08 14:13:14 -0600

    Implemented array search with linear

---
+++ Week5/5.Friday/Assignment-SearchAnalysis/array-search.js
@@ -0,0 +1,11 @@
+function search(array, item) {
+  let index = null;
+
+  for (let i = 0; i < array.length; i++) {
+    if (item === array[i]) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
