ef12c4d5206e48c1336f5662e79135e0e355c964
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-08 14:41:30 -0600

    running time added for quadratic time

--- Week5/5.Friday/Assignment-Quadratic/sum.js
+++ Week5/5.Friday/Assignment-Quadratic/sum.js
@@ -1,15 +1,21 @@
 // Quadratic Time function for sum
 function arrayContainsSum(array, sum) {
-  for (let i = 0; i < array.length; i++) {
-    const element1 = array[i];
+  for (let i = 0; // 1
+     i < array.length; // n + 1
+      i++) { // n
+    const element1 = array[i]; // n
 
-    for (let ii = 0; ii < array.length; i++) {
-      const element2 = array[ii];
+    for (let ii = 0; // 1
+         ii < array.length; // n + n^2 
+          i++) { // n^2
+      const element2 = array[ii]; // n^2
 
-      if (element1 + element2 === sum) {
+      if (element1 + element2 === sum)  // n^2
+      {
         return true;
       }
     }
   }
-  return false;
+  return false; // 2
 }
+// Running Time: 4n^2 + 5n + 4
