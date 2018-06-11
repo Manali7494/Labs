e3b9da85f474c896b0cdc53910406a36b308a70d
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-11 16:44:41 -0600

    Updated vampire js with few other tree traversal methods

--- Week5/4.Thursday/Assignment-Vampr/vampire.js
+++ Week5/4.Thursday/Assignment-Vampr/vampire.js
@@ -43,12 +43,45 @@ class Vampire {
   }
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
-  vampireWithName(name) {}
+  vampireWithName(name) {
+    if (name === this.name) {
+      return this;
+    }
+
+    for (let vamp of this.offspring) {
+      let search = vamp.vampireWithName(name);
+
+      if (search) {
+        return search;
+      }
+    }
+
+    return null;
+  }
 
   // Returns the total number of vampires that exist
-  get totalDescendents() {}
+  get totalDescendents() {
+    let vampSum = 0;
 
+    for (let vamp of this.offspring) {
+      vampSum += vamp.totalDescendents + 1;
+    }
+
+    return vampSum;
+  }
   // Returns an array of all the vampires that were converted after 1980
-  get allMillennialVampires() {}
+  get allMillennialVampires() {
+    let millenialVamps = []; // 1
+
+    if (this.yearConverted > 1980) {
+      millenialVamps.push(this); // 2
+    }
+
+    for (let vamp of this.offspring) {
+      millenialVamps = millenialVamps.concat(vamp.allMillennialVampires);
+    }
+
+    return millenialVamps;
+  }
 }
 module.exports = Vampire;
