884aff40c115d00994b3b32c81cec4f20a913b8d
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-19 14:04:46 -0600

    updated exercise1 to remove unused variables

--- Week7/2.Tuesday/ActiveRecordExercises/exercises/exercise_1.rb
+++ Week7/2.Tuesday/ActiveRecordExercises/exercises/exercise_1.rb
@@ -13,9 +13,9 @@ Output (puts) the number of the stores using ActiveRecord's count method,
  to ensure that there are three stores in the database.
 =end
 
-store1 = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-store2 = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-store3 = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 items = Store.all.count
 puts "There are #{items} stores in the database"
