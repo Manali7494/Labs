36b8ebc50491e179c0e7711f2ddee300fde757ac
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-19 14:28:30 -0600

    completed exercise4

--- Week7/2.Tuesday/ActiveRecordExercises/exercises/exercise_4.rb
+++ Week7/2.Tuesday/ActiveRecordExercises/exercises/exercise_4.rb
@@ -3,7 +3,21 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
-puts "Exercise 4"
-puts "----------"
+puts 'Exercise 4'
+puts '----------'
 
 # Your code goes here ...
+
+Store.create(name: 'Surrey', annual_revenue: 224_000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1_900_000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Gastown', annual_revenue: 430_000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |item|
+  puts "#{item.name} has an annual revenue of $ #{item.annual_revenue}"
+end
+
+@womens_store = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+@womens_store.each do |item|
+  puts "#{item.name} has an annual revene of #{item.annual_revenue}"
+end
