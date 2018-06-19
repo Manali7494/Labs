90ee88369f08252220d00a17770f59cda388bf55
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-19 14:40:21 -0600

    completed exercise5.rb

--- Week7/2.Tuesday/ActiveRecordExercises/exercises/exercise_5.rb
+++ Week7/2.Tuesday/ActiveRecordExercises/exercises/exercise_5.rb
@@ -4,7 +4,22 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+
+# Total Sum
+@total_sum = Store.sum('annual_revenue')
+puts "The total revenue of the entire company is #{@total_sum} dollars"
+
+# Average annual revenue
+@store_num = Store.all.count
+@average = @total_sum / @store_num
+puts "The average annual revenue for all stores is #{@average}"
+
+#Stores generating more than $1M+ annual sales
+
+@rich_stores = Store.where('annual_revenue > ?', 1000000).count()
+
+puts "There are #{@rich_stores} stores with an annual revenue greater than $1M"
