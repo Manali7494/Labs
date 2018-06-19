209e0b994a57e217fa265e53cca0490df74aa58c
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-19 16:18:15 -0600

    completed exercise 7

--- Week7/2.Tuesday/ActiveRecordExercises/exercises/exercise_7.rb
+++ Week7/2.Tuesday/ActiveRecordExercises/exercises/exercise_7.rb
@@ -6,7 +6,33 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
 # Your code goes here ...
+# NOTES:
+#  Check if not empty --> validates_presence_of :item
+# Check if a nnumber --> validates_numericality_of :item
+# Check if unique --> validates_uniqueness_of :item
+# If two fields need to be equal --> validates_confirmation_of :item (ie password)
+# Required checkbox checked --> validates_acceptance_of
+# Check length --> validates_length_of :password, minimum: 3
+# regular expression ie email is valid--> validates_format_of :email , with: /regex/i
+# Check between two values --> validates_inclusion_of :age, in: 21..99
+# Check not included in range --> validates_exclusion of :age, in: 0..21, message: "sorry you must be over 21"
+#
+# easier sytax:
+# validates :status, presence :true, length: {minimum: 3}
+# t = Tweet.new
+# t.save
+# t.errors.messages {hash }
+# t.errors[:status][0] --> can't be blank
+
+puts 'Please enter a store name'
+print '> '
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+error_messages = new_store.errors.messages
+
+p "Name Error: #{error_messages[:name][0]}"
+p "Revenue Error: #{error_messages[:annual_revenue][0]}"
--- Week7/2.Tuesday/ActiveRecordExercises/lib/store.rb
+++ Week7/2.Tuesday/ActiveRecordExercises/lib/store.rb
@@ -1,3 +1,7 @@
 class Store < ActiveRecord::Base
-
+  has_many :employees
+  validates :name, length: { minimum: 3,
+                             too_short: 'Must have a minimum of 3 characters for names' }
+  validates :annual_revenue, numericality:
+ { greater_than: 0, message: 'Must have an annual revenue greater than 0' }
 end
